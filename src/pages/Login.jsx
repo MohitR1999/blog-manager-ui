a78c4d18c9b340ac11e0d841a1a517dd0af26842
MohitR1999 <mohitr1999@gmail.com>
2025-01-30 20:37:34 +0530

    Added reducer logic in the store and removed the extra code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,51 @@
-import { Card, Col, Form, Input, Row, Checkbox, Button, Flex } from "antd";
+import { Card, Col, Form, Input, Row, Checkbox, Button, Flex, Spin } from "antd";
 import Title from "antd/es/typography/Title";
-import React from "react";
+import React, { useState } from "react";
+import { login } from '../store/slices/userAuth';
+import { useDispatch } from "react-redux";
+
+const HOST = "http://localhost"
+const PORT = "8080"
 
 const Login = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+
+
+    const submitHandler = async () => {
+        if (userName && password) {
+            try {
+                setLoading(true);
+                const response = await fetch(`${HOST}:${PORT}/auth/login`, {
+                    method : 'POST',
+                    body : JSON.stringify({
+                        username : userName,
+                        password : password
+                    }),
+                    headers : {
+                        'Content-Type' : 'application/json'
+                    }
+                });
+                const result = await response.json();
+                if (result.token) {
+                    dispatch(login(result.token))
+                }
+                setLoading(false);
+
+            } catch (error) {
+                console.log('An error occured!');
+                console.log(error);
+                setLoading(false);
+            }
+            
+        }
+    }
+    
+    
     return (
-        <Row justify="center" align="top">
+        <Row style={{ height : '100%' }} justify="center" align="middle">
             <Col span={24}>
                 <Flex justify="center" align="center">
                     <Card>
@@ -20,7 +61,7 @@ const Login = () => {
                                     },
                                 ]}
                             >
-                                <Input />
+                                <Input value={userName} onChange={(e) => setUserName(e.target.value)} />
                             </Form.Item>
 
                             <Form.Item
@@ -33,13 +74,14 @@ const Login = () => {
                                     },
                                 ]}
                             >
-                                <Input.Password />
+                                <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} />
                             </Form.Item>
 
                             <Form.Item label={null}>
-                                <Button type="primary" htmlType="submit">
+                                <Button type="primary" htmlType="submit" onClick={submitHandler} disabled={loading}>
                                     Submit
                                 </Button>
+                                { loading ? <Spin/> : <></> }
                             </Form.Item>
                         </Form>
                     </Card>
--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './slices/userAuth';
 
+export default configureStore({
+    reducer : {
+        userAuth : authReducer
+    }
+})
